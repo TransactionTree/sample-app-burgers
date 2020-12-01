b8acda59a19e7f829044a7c1acc5d5f026889496
Kevin Guebert <kevin.guebert@ncr.com>
2020-12-01 12:37:51 -0500

    Removing Login/Logout/Register UI, Code Cleanup

--- code/burger/urls.py
+++ code/burger/urls.py
@@ -3,13 +3,13 @@ from burger import views
 
 urlpatterns = [
 
-path('',views.index, name='index'),
-path('findRestaurant',views.findRestaurant, name = 'findRestaurant'),
-path('midtownMenu', views.midtownMenu, name = 'midtownMenu'),
-path('southlandMenu',views.southlandMenu, name='southlandMenu'),
-path('highlandsMenu',views.highlandsMenu,name='highlandsMenu'),
-path('payment/', views.payment, name='payment'),
-path('viewCart', views.viewCart, name='viewCart'),
-path('confirmation',views.confirmation, name='confirmation'),
-path('about', views.about, name='about'),
+    path('', views.index, name='index'),
+    path('findRestaurant', views.findRestaurant, name='findRestaurant'),
+    path('midtownMenu', views.midtownMenu, name='midtownMenu'),
+    path('southlandMenu', views.southlandMenu, name='southlandMenu'),
+    path('highlandsMenu', views.highlandsMenu, name='highlandsMenu'),
+    path('payment/', views.payment, name='payment'),
+    path('viewCart', views.viewCart, name='viewCart'),
+    path('confirmation', views.confirmation, name='confirmation'),
+    path('about', views.about, name='about'),
 ]
