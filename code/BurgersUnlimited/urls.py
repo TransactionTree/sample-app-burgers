327502ee3e90fedb7c2b0286921e716177b3c1a7
Kevin Guebert <kevin.guebert@ncr.com>
2020-12-02 15:54:40 -0500

    Removing commented code

--- code/BurgersUnlimited/urls.py
+++ code/BurgersUnlimited/urls.py
@@ -23,14 +23,9 @@ from django.contrib.auth import views as auth_v
 from django.urls import include, path
 from django.views.generic import RedirectView
 
-# from users import views as user_v
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('burger/', include('burger.urls'), name='main-burger'),
     path('', RedirectView.as_view(url='burger/')),
     path('__debug__/', include(debug_toolbar.urls)),
-    # path('Register/', user_v.register, name='Register'),
-    # path('login/', auth_v.LoginView.as_view(template_name='users/login.html'), name='login'),
-    # path('logout/', auth_v.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
